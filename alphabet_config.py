f9409a591fc90fa3f4bdb5f8e2cd2ef4fdb90f58
yashar paymai <yasharp1383@gmail.com>
2025-04-17 22:12:09 +0330

    ascii bug fixed

--- alphabet_config.py
+++ alphabet_config.py
@@ -5,10 +5,10 @@ White_spaces = [chr(32),chr(10),chr(9),chr(13),chr(11),chr(12)]
 Digits = [chr(i) for i in range(48,58)]
 English_aphabet = [chr(i) for i in (list(range(65,91)) + list(range(97,123)))]
 Illegal = []
-for i in range(128):
+for i in range(256):
     if (chr(i) not in Symbols) and (chr(i) not in White_spaces) and (chr(i) not in Digits) and (chr(i) not in English_aphabet) :
         Illegal.append(chr(i))
-sigma = [chr(i) for i in range(128)]
+sigma = [chr(i) for i in range(256)]
 
 
 def is_keyword(token): 
